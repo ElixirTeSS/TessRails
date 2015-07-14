91a785b9f4defa980fcdd4952a64dfbd65dae19f
Milo Thurston <knirirr@gmail.com>
2015-07-14 12:15:06 +0100

    Fixed flash layout.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
 
-  def initialize(attributes = {})
-    @name  = attributes[:name]
-    @email = attributes[:email]
-  end
+  #def initialize(attributes = {})
+  #  @name  = attributes[:name]
+  #  @email = attributes[:email]
+  #end
 
   def formatted_email
     "#{@name} <#{@email}>"
